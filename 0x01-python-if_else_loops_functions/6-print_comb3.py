d6c63897b1a73dfc8b1a4632b03c53bdb824fa98
Francesca Cantor <fpcantor@gmail.com>
2018-09-16 05:44:32 +0000

    Added py script for task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for n in range(0, 9):
+    for i in range(n + 1, 10):
+        if n < 8:
+            print("{:d}{:d}, ".format(n, i), end="")
+        else:
+            print("{:d}{:d}".format(n, i))
