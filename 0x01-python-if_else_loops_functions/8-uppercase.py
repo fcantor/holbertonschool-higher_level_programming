c710b6455c46cabf8df643f80a2f95c74f76cf1a
Francesca Cantor <fpcantor@gmail.com>
2018-09-16 07:09:45 +0000

    Refactored script to simplify logic

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for alpha in str:
-        odd = 0
-        if ord(alpha) > 96 and ord(alpha) < 123:  # if letter is lowercase
-            odd = -32
-        print("{:s}".format(chr(ord(alpha) + odd)), end='')  # print upper
-    else:
-        print()
+        if ord(alpha) >= ord('a') and ord(alpha) <= ord('z'):
+            alpha = chr(ord(alpha) - 32)
+        print("{:s}".format(alpha), end="")
+    print("")
