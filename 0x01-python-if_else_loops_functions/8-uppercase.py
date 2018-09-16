ffe8be0100533b5bb414f4e7b90d85902a714b26
Francesca Cantor <fpcantor@gmail.com>
2018-09-16 06:35:49 +0000

    Fixed comment styling

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     for alpha in str:
         odd = 0
-        if ord(alpha) > 96 and ord(alpha) < 123: # if letter is lowercase
+        if ord(alpha) > 96 and ord(alpha) < 123:  # if letter is lowercase
             odd = -32
-        print("{:s}".format(chr(ord(alpha) + odd)), end='') # print upper
+        print("{:s}".format(chr(ord(alpha) + odd)), end='')  # print upper
     else:
         print()
