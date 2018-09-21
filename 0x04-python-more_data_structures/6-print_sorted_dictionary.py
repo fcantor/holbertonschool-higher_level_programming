1e7d95dc4f03aaa940a1ac701c88fb0a2e277edf
Francesca Cantor <fpcantor@gmail.com>
2018-09-21 03:57:31 +0000

    Added sort method to list

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for x, y in a_dictionary.items():
-        print("{}: {}".format(x, y))
+    [print("{}: {}".format(k,v)) for k, v in sorted(a_dictionary.items())]
