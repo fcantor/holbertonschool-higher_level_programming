280d556c81fe6868da89f17423277557f5287505
Francesca Cantor <fpcantor@gmail.com>
2018-09-21 05:35:34 +0000

    Removed variable i

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,6 @@
 def best_score(a_dictionary):
     '''Returns a key with the biggest integer value'''
     if a_dictionary:
-        i = sorted(a_dictionary.keys())[-1]
-        return i
+        return sorted(a_dictionary.keys())[-1]
     else:
         return None
