49884eb296cd2bd5b6bc6669cd60090843a04bbe
Francesca Cantor <fpcantor@gmail.com>
2018-10-16 07:06:30 +0000

    Fixed pep8 styling

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,6 +10,7 @@ class BaseGeometry:
     '''
     def area(self):
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
