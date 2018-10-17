68733ad982a1ca870ffac5653f9ad12647c300cc
Francesca Cantor <fpcantor@gmail.com>
2018-10-17 02:58:14 +0000

    Added solution to task 10

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+class_to_json function
+'''
+import json
+
+
+def class_to_json(obj):
+    '''
+    Returns the dictionary description with simple data structure for
+    JSON serialization of an object
+    '''
+    return obj.__dict__
