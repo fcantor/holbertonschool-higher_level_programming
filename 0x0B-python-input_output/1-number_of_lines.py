fe2ccfc620cec28758bb8504b0357f67e3772b84
Francesca Cantor <fpcantor@gmail.com>
2018-10-17 03:57:19 +0000

    Fixed pep8 styling

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -9,7 +9,7 @@ def number_of_lines(filename=""):
     Returns the number of lines of a text file
     '''
     i = 0
-    with open(filename, encoding = 'utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         for line in f:
             i += 1
         return i
