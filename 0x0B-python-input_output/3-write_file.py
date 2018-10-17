d389c1ef0c70a5435b03fb52f2aaecb4bb30b6f0
Francesca Cantor <fpcantor@gmail.com>
2018-10-17 03:59:21 +0000

    Fixed pep8 styling

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -9,7 +9,7 @@ def write_file(filename="", text=""):
     Writes a string to a text file (UTF8) and returns the number
     of characters written
     '''
-    with open(filename, mode = 'w', encoding = 'utf-8') as f:
+    with open(filename, mode='w', encoding='utf-8') as f:
         new_file = f.write(text)
 
     return new_file
