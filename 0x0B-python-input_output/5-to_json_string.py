88a83f909068e99d2cd97a70aa09320ce97fd584
Francesca Cantor <fpcantor@gmail.com>
2018-10-17 04:08:59 +0000

    Added paranthesis around return

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -2,11 +2,11 @@
 '''
 to_json_string function
 '''
-
 import json
+
 
 def to_json_string(my_obj):
     '''
     Returns the JSON representation of an object
     '''
-    return json.dumps(my_obj)
+    return (json.dumps(my_obj))
