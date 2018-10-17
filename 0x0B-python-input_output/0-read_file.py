66ae005e3594e6773cde4b75af9daee7fdbdd4a6
Francesca Cantor <fpcantor@gmail.com>
2018-10-17 03:55:13 +0000

    Changed logic, added for loop

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,5 @@ def read_file(filename=""):
     Reads a text file (UTF8) and prints to stdout
     '''
     with open(filename, encoding='utf-8') as f:
-        print(f.read())
+        for line in f:
+            print(line, end="")
