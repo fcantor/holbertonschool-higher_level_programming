a0ed2768ea9778616563fa4f2f9c946a52354fd9
Francesca Cantor <fpcantor@gmail.com>
2018-10-23 05:46:53 +0000

    Added size getter and setter methods

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,5 +12,18 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        ''' prints to stdout the rectangle info in a different format '''
         return ("[Square] ({:d}) {:d}/{:d} - {:d}"
                 .format(self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        ''' size value getter '''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        ''' size value setter '''
+        self.width = value
+        self.height = value
+    
