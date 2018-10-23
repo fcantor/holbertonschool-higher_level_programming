455f171bcdc0d5a9adeedb305ef8a742d1375ef3
Francesca Cantor <fpcantor@gmail.com>
2018-10-23 04:29:52 +0000

    Added square module

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+class Square module
+'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    ''' Square class which inherits from Rectangle class '''
+    def __init__(self, size, x=0, y=0, id=None):
+        ''' initialize Square class '''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return ("[Square] ({:d}) {:d}/{:d} - {:d}"
+            .format(self.id, self.x, self.y, self.width))
+        
