b15f5e3b4a40c22b560687dae83c45fa64c7329e
Francesca Cantor <fpcantor@gmail.com>
2018-12-04 06:10:03 +0000

    Added solution to task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- This script creates a table second_table and adds multiple rows
+-- second_table values: id INT, name VARCHAR(256), score INT
+-- If the table exists, script will not output an error
+CREATE TABLE IF NOT EXISTS second_table(
+   id INT,
+   name VARCHAR(256),
+   score INT
+);
+
+INSERT INTO `second_table`
+       (`id`, `name`, `score`)
+VALUES
+	(1,"John",10),
+	(2,"Alex",3),
+	(3,"Bob",14),
+	(4,"George",8);
