31ed796634871e90384751b4fdc7a0a2e7589931
Francesca Cantor <fpcantor@gmail.com>
2018-12-04 06:37:44 +0000

    Added solution to task 11

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- This script lists all records with a score >= 10 in second_table
+-- Displays score & name; ordered by score
+SELECT score,name FROM second_table WHERE score >= 10 ORDER BY score DESC;
