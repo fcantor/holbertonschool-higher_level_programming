500f9f0edf15ae26420a72696956a5c6cfa2223e
Francesca Cantor <fpcantor@gmail.com>
2018-12-06 00:49:13 +0000

    Fixed: Added comma for correct syntax in FOREIGN KEY

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,8 @@
 -- cities values: id INT, state_id INT, name VARCHAR(256)
 -- If database or table already exists, no error will occur
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
-    state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id),
+    state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL
 );
