bb50a99e0acabf1fd18d0fb6113e234b67a9e206
Francesca <fpcantor@gmail.com>
2019-01-06 02:37:26 +0000

    Fixed port string to integer

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     from sys import argv
 
     db = MySQLdb.connect(host="localhost",
-    					port="3306",
+    					port=3306,
     					user=argv[1],
     					passwd=argv[2],
     					db=argv[3])
