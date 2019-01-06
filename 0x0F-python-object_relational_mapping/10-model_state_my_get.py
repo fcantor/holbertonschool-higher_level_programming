d2b38d8022e73ccde8819d7c74490bb38028ad47
Francesca <fpcantor@gmail.com>
2019-01-06 17:26:34 +0000

    Added solution to task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Script that lists all State object with the name passed
+from the database hbtn_0e_6_usa
+'''
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    ''' create an instance of Engine=core interface to the database '''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    '''pool_pre_ping tests connections for liveness upon each checkout'''
+
+    ''' create a session '''
+    session = sessionmaker(bind=engine)()
+
+    ''' query object which loads instances of State '''
+    result = session.query(State).filter(State.name == argv[4]).all()
+    if result:
+        for row in result:
+            if row.name == argv[4]:
+                print("{}".format(row.id))
+    else:
+        print("Not found")
+    session.close()
