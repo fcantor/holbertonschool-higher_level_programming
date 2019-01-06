ff7cbc71fcc8cb35de6ee361ab2d01bef3f87c57
Francesca <fpcantor@gmail.com>
2019-01-06 03:13:18 +0000

    Added mysql command

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     ''' execute SQL queries '''
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
 
     ''' fetch all-at-once '''
     rows = cur.fetchall()
