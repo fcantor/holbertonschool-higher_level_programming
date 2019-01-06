bdf00360bbf4837feb7e95b0e3906f0e6d533e33
Francesca <fpcantor@gmail.com>
2019-01-06 02:39:55 +0000

    Re-added comments

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,13 +5,20 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
+    ''' connects to the database '''
     db = MySQLdb.connect(host="localhost",
-    					port=3306,
-    					user=argv[1],
-    					passwd=argv[2],
-    					db=argv[3])
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    ''' cursor for multiple separate working envs thru one connection '''
     cur = db.cursor()
+
+    ''' execute SQL queries '''
     cur.execute("SELECT * FROM states ORDER BY id")
+
+    ''' fetch all-at-once '''
     rows = cur.fetchall()
     for col in rows:
         print("{}".format(col))
