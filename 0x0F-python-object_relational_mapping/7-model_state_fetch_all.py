c9ad178f86b7c104d7d41e669c579b6710fb9ce0
Francesca <fpcantor@gmail.com>
2019-01-06 16:51:38 +0000

    Fixed spacing and added import statement

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' Script that lists all State objects from the database hbtn_0e_6_usa '''
 
+import sys
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -12,8 +13,10 @@ if __name__ == "__main__":
                            format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     '''pool_pre_ping tests connections for liveness upon each checkout'''
+
     ''' create a session '''
     session = sessionmaker(bind=engine)()
+
     ''' query object which loads instances of State '''
     result = session.query(State).all()
     for row in result:
