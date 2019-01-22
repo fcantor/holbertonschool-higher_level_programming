1318ccaf4ab2e3f4cacbf9c9d3d56c2bd42f720a
Francesca <fpcantor@gmail.com>
2019-01-22 08:25:09 +0000

    Initial commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+from requests import post
+from sys import argv
+
+
+if __name__ == "__main__":
+    q = ""
+    if len(argv) > 1:
+        q = argv[1]
+    try:
+        resp = post("http://0.0.0.0:5000/search_user", data={'q': q}).json()
+    except:
+        print("Not a valid JSON")
+    if resp:
+        print("[{}] {}".format(resp['id'], resp['name']))
+    else:
+        print("No result")
