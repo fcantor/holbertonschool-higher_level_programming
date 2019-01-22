49ae49f02dd4cf78717de9dbc766a7cb2ede0836
Francesca <fpcantor@gmail.com>
2019-01-22 08:28:07 +0000

    Took post request outside of try

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     q = ""
     if len(argv) > 1:
         q = argv[1]
+    resp = post("http://0.0.0.0:5000/search_user", data={'q': q})
     try:
-        resp = post("http://0.0.0.0:5000/search_user", data={'q': q}).json()
+        json = resp.json()
     except:
         print("Not a valid JSON")
     if resp:
