44baa5984a9ed0ec3a9b77f3ec1b8cf293719230
Francesca <fpcantor@gmail.com>
2019-01-22 08:43:33 +0000

    Put post request back, put if/else statement under else

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,12 +11,12 @@ if __name__ == "__main__":
     q = ""
     if len(argv) > 1:
         q = argv[1]
-    resp = post("http://0.0.0.0:5000/search_user", data={'q': q})
     try:
-        f = resp.json()
+        resp = post("http://0.0.0.0:5000/search_user", data={'q': q}).json()
     except:
         print("Not a valid JSON")
-    if f:
-        print("[{}] {}".format(f['id'], f['name']))
     else:
-        print("No result")
+        if resp:
+            print("[{}] {}".format(resp['id'], resp['name']))
+        else:
+            print("No result")
