dfdf97a2d9567296cbdaf6cdc4c21dee61100b31
Francesca <fpcantor@gmail.com>
2019-02-20 01:08:57 +0000

    Fixed semistandard styling

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Script that prints two arguments passed to it, in the following format: “ is ”
-if (!process.argv[2] && ! process.argv[3]) {
+if (!process.argv[2] && !process.argv[3]) {
   console.log('undefined is undefined');
 } else if (process.argv[2] && process.argv[3]) {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
