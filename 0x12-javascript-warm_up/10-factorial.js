a91fb7f3d60ee8f9a5d73474419470611f2cc522
Francesca <fpcantor@gmail.com>
2019-02-24 23:21:40 +0000

    Added case for checking it num is NaN

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 // Script that computes and prints a factorial
 let factorial = function (num) {
+  if (isNaN(Number(num))) {
+    return (1);
+  }
   return num ? num * factorial(num - 1): 1;
 };
 
