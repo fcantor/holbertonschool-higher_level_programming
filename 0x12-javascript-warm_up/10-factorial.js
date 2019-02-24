1e552cded4da308d2a0f40eed09a46e1c1e5e2c3
Francesca <fpcantor@gmail.com>
2019-02-24 23:19:44 +0000

    Refactored using conditional ternary operator

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 // Script that computes and prints a factorial
 let factorial = function (num) {
-  let list = [];
-  if (!num || isNaN(Number(num))) {
-    return (1);
-  } else if (list[num] > 0) {
-    return (list[num]);
-  }
-  return (list[num] = factorial(num - 1) * num);
+  return num ? num * factorial(num - 1): 1;
 };
 
 let result = factorial(process.argv[2]);
