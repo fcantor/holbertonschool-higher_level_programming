196ee697330690aad8b8f8ece912e8ef4ec7041b
Francesca <fpcantor@gmail.com>
2019-03-29 00:18:48 +0000

    Fixed missing parenthesis

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -2,8 +2,8 @@
 $(document).ready(function () {
   let url = 'https://fourtonfish.com/hellosalut/?lang=';
   $('#btn_translate').click(function () {
-    let language = $('#language_code').val();
-    $.get(url + language, function (data) {
+    let code = $('#language_code').val();
+    $.get(url + code, function (data) {
       $('#hello').append('<div>' + data.hello + '</div>');
     });
   });
